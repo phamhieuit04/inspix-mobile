2c112621d95a28fc9aa9757a0b4a95bd3d29ff9e
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-04-16 09:36:15 +0700

    Rename ui to presentation for better struct

--- app/src/main/java/com/example/inspixmobile/MainActivity.kt
+++ app/src/main/java/com/example/inspixmobile/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.inspixmobile.ui.theme.InspixMobileTheme
+import com.example.inspixmobile.presentation.theme.InspixMobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/inspixmobile/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/inspixmobile/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.inspixmobile.ui.theme
+package com.example.inspixmobile.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
