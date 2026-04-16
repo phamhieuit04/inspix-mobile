dd739621779a20b67828b915638bc40d76ce3059
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-04-16 10:35:12 +0700

    Add base home screen + update nav bar

--- app/src/main/java/com/example/inspixmobile/presentation/component/NavigationBar.kt
+++ app/src/main/java/com/example/inspixmobile/presentation/component/NavigationBar.kt
@@ -1,9 +1,22 @@
 package com.example.inspixmobile.presentation.component
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation3.runtime.NavKey
 
 data class BottomNavItem(
     val label: String,
     val icon: ImageVector,
     val selectedIcon: ImageVector
 )
+
+@Composable
+fun NavigationBar(
+    modifier: Modifier = Modifier,
+    isVisible: Boolean = true,
+    selectedKey: NavKey,
+    onSelectKey: (NavKey) -> Unit,
+) {
+    if (!isVisible) return
+}
--- app/src/main/java/com/example/inspixmobile/presentation/navigation/Graph.kt
+++ app/src/main/java/com/example/inspixmobile/presentation/navigation/Graph.kt
@@ -3,12 +3,16 @@ package com.example.inspixmobile.presentation.navigation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.example.inspixmobile.core.util.Navigator
+import com.example.inspixmobile.presentation.component.NavigationBar
+import com.example.inspixmobile.presentation.screen.HomeScreen
 import com.example.inspixmobile.presentation.state.rememberNavigationState
 import com.example.inspixmobile.presentation.state.toEntries
 
@@ -22,17 +26,36 @@ fun Graph() {
     val navigator = remember { Navigator(navigationState) }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
     ) {
         NavDisplay(
             onBack = navigator::goBack,
             entries = navigationState.toEntries(
                 entryProvider {
                     entry<Destination.Home> {
+                        HomeScreen()
+                    }
+                    entry<Destination.Search> {
+
+                    }
+                    entry<Destination.Upload> {
+
+                    }
+                    entry<Destination.Profile> {
 
                     }
                 }
             )
+        )
+
+        NavigationBar(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            selectedKey = navigationState.topLevelRoute,
+            onSelectKey = {
+                navigator.navigate(it)
+            }
         )
     }
 }
