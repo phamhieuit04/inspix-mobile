738246a4cc986e24399d2b56c2b3ee0d72d86d0f
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-04-19 16:05:46 +0700

    Remove home ui state

--- app/src/main/java/com/example/inspixmobile/presentation/state/HomeUiState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.inspixmobile.presentation.state
-
-data class HomeUiState(
-    val isRefreshing: Boolean = false,
-    val isInitialLoading: Boolean = true
-)
--- app/src/main/java/com/example/inspixmobile/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/inspixmobile/presentation/viewmodel/HomeViewModel.kt
@@ -6,19 +6,11 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.inspixmobile.domain.contract.repository.ICollectionRepository
 import com.example.inspixmobile.domain.model.Collection
-import com.example.inspixmobile.presentation.state.HomeUiState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class HomeViewModel(
     private val collectionRepository: ICollectionRepository
 ) : ViewModel() {
-
-    private val _uiState = MutableStateFlow(HomeUiState())
-    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
     fun getCollectionsPaging(
         pageSize: Int,
@@ -27,13 +19,5 @@ class HomeViewModel(
         return collectionRepository
             .getCollectionsPaging(pageSize = pageSize, prefetchDistance = prefetchDistance)
             .cachedIn(viewModelScope)
-    }
-
-    fun onRefreshing(isRefreshing: Boolean) {
-        _uiState.update { it.copy(isRefreshing = isRefreshing) }
-    }
-
-    fun onInitialLoading(isInitialLoading: Boolean) {
-        _uiState.update { it.copy(isInitialLoading = isInitialLoading) }
     }
 }
