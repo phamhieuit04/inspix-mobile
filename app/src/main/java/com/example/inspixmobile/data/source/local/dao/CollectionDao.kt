196fef6fbaad38cfddea9be437631e6fee8a8d89
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-06-19 10:51:31 +0700

    wip

--- app/src/main/java/com/example/inspixmobile/data/source/local/dao/CollectionDao.kt
+++ app/src/main/java/com/example/inspixmobile/data/source/local/dao/CollectionDao.kt
@@ -68,11 +68,11 @@ interface CollectionDao {
     suspend fun resetLikedCollections()
 
     @Transaction
-    @Query("SELECT * FROM collections WHERE is_liked = 1 ORDER BY updated_at ASC")
+    @Query("SELECT * FROM collections WHERE is_liked = 1 ORDER BY updated_at DESC")
     fun getLikedCollectionsPagingSource(): PagingSource<Int, CollectionWithImagesAndAuthor>
 
     @Transaction
-    @Query("SELECT * FROM collections WHERE user_uuid = :userUuid ORDER BY updated_at ASC")
+    @Query("SELECT * FROM collections WHERE user_uuid = :userUuid ORDER BY updated_at DESC")
     fun getOwnedCollectionsPagingSource(userUuid: String): PagingSource<Int, CollectionWithImagesAndAuthor>
 
     @Transaction
