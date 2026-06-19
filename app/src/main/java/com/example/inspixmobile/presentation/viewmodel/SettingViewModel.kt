7090ccf01157c0bf7df547edb9caabfdbd13d9b3
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-06-19 22:31:38 +0700

    Add sign out loading

--- app/src/main/java/com/example/inspixmobile/presentation/viewmodel/SettingViewModel.kt
+++ app/src/main/java/com/example/inspixmobile/presentation/viewmodel/SettingViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.inspixmobile.presentation.viewmodel
 
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.inspixmobile.data.source.local.store.SettingStore
 import com.example.inspixmobile.domain.contract.repository.IAuthRepository
 import com.example.inspixmobile.presentation.component.NavigationBarStyle
 import com.example.inspixmobile.presentation.screen.HomeLayoutStyle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
@@ -13,6 +17,9 @@ class SettingViewModel(
     private val settingStore: SettingStore,
     private val authRepository: IAuthRepository
 ) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
 
     fun updateHomeLayout(layout: HomeLayoutStyle) {
         viewModelScope.launch {
@@ -34,7 +41,12 @@ class SettingViewModel(
 
     fun logout() {
         viewModelScope.launch {
+            _isLoading.value = true
+
+            delay(500)
             authRepository.logout()
+
+            _isLoading.value = false
         }
     }
 }
