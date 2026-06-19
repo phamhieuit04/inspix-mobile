c2a1c1dfd61305f0fa7b2ed9411252af919a35fd
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2026-06-19 10:41:48 +0700

    Handle unsplash author information

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.browser)
 
     // compose
     implementation(platform(libs.androidx.compose.bom))
